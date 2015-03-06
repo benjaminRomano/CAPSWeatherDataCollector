7ec08e6b712b0bb6dcb9e50b603567b9873c90d0
BenjaminRomano <bromano@crimson.ua.edu>
2015-03-06 14:31:48 -0600

    Oops forgot to add these files...

---
+++ CAPSDataWall/WeatherAPIModels/StreamDescriptions/KMLStreamDescription.cs
@@ -0,0 +1,19 @@
+﻿using WeatherAPIModels.Models;
+using WeatherAPIModels.StreamNames;
+
+namespace WeatherAPIModels.StreamDescriptions
+{
+    public class KMLStreamDescription
+    {
+        public KMLDataSource Source { get; set; }
+        public KMLDataType KMLDataType { get; set; }
+        public BaseStreamName StreamName { get; set; }
+
+        public KMLStreamDescription(KMLDataSource source, KMLDataType kmlDataType, BaseStreamName streamName)
+        {
+            this.Source = source;
+            this.KMLDataType = kmlDataType;
+            this.StreamName = streamName;
+        }
+    }
+}
